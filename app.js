0a5884c5edd1f669e1d44a032fa981683401d4ab
44tim44 <timeisrelative94@gmail.com>
2019-10-03 16:15:48 +0200

    Added Spotify Examples

--- app.js
+++ app.js
@@ -6,6 +6,8 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var mobiletestRouter = require('./routes/mobiletest');
+var spotifyRouter = require('./routes/spotify');
 
 var app = express();
 var port = 3000;
@@ -28,6 +30,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/mobiletest', mobiletestRouter);
+app.use('/spotify', spotifyRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -42,7 +46,7 @@ app.use(function(err, req, res, next) {
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  res.render('error', { errmsg: err.message });
 });
 
 module.exports = app;
