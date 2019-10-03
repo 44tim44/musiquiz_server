861abc4c04d7558c911380fe119f5d2f1c79db0f
44tim44 <timeisrelative94@gmail.com>
2019-10-03 14:49:28 +0200

    Removed Jade

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,6 @@
 var express = require('express');
 var router = express.Router();
-/*
-exports.index = function(req, res){
-  res.render('index', { title: 'ejs' });};
-*/
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index.html', { title: 'Musiquiz' });
