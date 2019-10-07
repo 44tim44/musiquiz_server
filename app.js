0e5da08e55e7c790360ae05ae1ae16dc62029673
magnusemanuelsson <magnus.emanuelsson@hotmail.se>
2019-10-07 15:22:53 +0200

    la till ansluta till en server och skicka info

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var mobiletestRouter = require('./routes/mobiletest');
 var spotifyRouter = require('./routes/spotify');
+var connectRouter = require('./routes/connect');
 
 var app = express();
 var port = 3000;
@@ -32,6 +33,8 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/mobiletest', mobiletestRouter);
 app.use('/spotify', spotifyRouter);
+app.use('/connect', connectRouter);
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
